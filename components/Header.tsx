4fdbe3eb5724d8302a8bbb7f9aa867708543e59e
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-09-05 14:57:50 +0200

    ♻️ Adjust header, seperate Header and Footer styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,10 +7,10 @@ export default function Header() {
     <header className="site-header">
       <h1 className="h3 site-header__title">
         <span className="site-header__logo">🎞️</span>
-        <Link
-          href="/"
-          className="site-header__link"
-        >{`Filme ${currentYear}`}</Link>
+        <Link href="/" className="site-header__link">
+          Das Jahr <span className="site-header__year">{currentYear}</span> in
+          Filmen
+        </Link>
       </h1>
     </header>
   );
